8d0097ee5fd3aa6d12e54090617b662d16d14ae9
Bryan <bryan.cduran@alumnos.upm.es>
2023-03-18 00:21:38 +0100

    Add photo

--- src/pages/ContactPage/ContactComponent.tsx
+++ src/pages/ContactPage/ContactComponent.tsx
@@ -70,11 +70,14 @@ export function ContactComponent({
         >
           <VStack spacing='2.5' width='100%'>
             <Image
-              width='100%'
+              width='auto'
               height='64'
-              src='./avatar.png'
+              src='./contact/BryanSCD.png'
               objectFit='scale-down'
               objectPosition='center'
+              bgColor='whiteAlpha.800'
+              rounded="xl"
+              shadow="xl"
             />
 
             <Text fontSize='lg' width='100%' color='white' textAlign='center'>
