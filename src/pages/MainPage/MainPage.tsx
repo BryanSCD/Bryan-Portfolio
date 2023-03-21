f84cf6be15ae1fdd59f648a094209be2a7059542
Bryan <bryan.cduran@alumnos.upm.es>
2023-03-21 08:50:43 +0100

    Add scroll down

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,8 +5,8 @@ import { VStack, forwardRef } from '@chakra-ui/react';
 
 import { Page, PageProps } from '../../components';
 import { AvatarLayer } from './AvatarLayer';
-import { Layer } from './Layer';
 import { ParticlesLayer } from './ParticlesLayer';
+import { TouchLayer } from './TouchLayer';
 
 export const MainPage = forwardRef<PageProps, 'div'>(({ ...rest }, ref) => {
   const divAvatar = useRef<HTMLDivElement>(null);
@@ -27,7 +27,7 @@ export const MainPage = forwardRef<PageProps, 'div'>(({ ...rest }, ref) => {
         {/* Avatar layer */}
         {pageInView && AvatarLayerRendered}
         {/* Touch layer */}
-        <Layer
+        <TouchLayer
           zIndex={30}
           onClickCapture={(e) => {
             const event = new MouseEvent('click', e.nativeEvent);
